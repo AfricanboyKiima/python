fd9248df2f54e776d783faf78905f2b46a67150a
Africanboy Kiima <fadhilkiima@gmail.com>
2023-01-10 08:02:04 +0100

    Understood how to make a commit related within a child folder so made this commit

--- OOP/classattributes.py
+++ OOP/classattributes.py
@@ -2,6 +2,7 @@
 
 
 class Item:#class is a model which simply means it represents an idea or theory 
+    pay_rate = 0.8#class attribute
     def __init__(self,name:str, price:float,quantity = 0):#constructor to create attributes of our object
 
         assert price >= 0, f"Price {price} is not greater than or equal to zero "
@@ -16,4 +17,11 @@ class Item:#class is a model which simply means it represents an idea or theory
 
         return self.price * self.quantity
 
-    
+
+item1 = Item("Phone",100,1)
+item2 = Item("Laptop",1000,3)
+
+
+print(Item.__dict__)#this gives all the attributes at the class level
+print(item1.__dict__)
+print(item2.__dict__)
--- OOP/dustbin1.py
+++ OOP/dustbin1.py
@@ -1,4 +1,18 @@
 class Item: 
+    """This thing works like global and local scopes remember when we are working with scopes variables within the 
+    the functions are said to exist in the local scope while variables outside functions are said to exist 
+    in the global scope. A scope is like a container for variables and remember when dealing with the local scope,
+    it is destroyed when the functions returns such that the values that were assigned to the variables are
+    forgotten and it is for this reason that variables outside functions can not access the values of the 
+    variables within functions since these values are forgotten when the function call returns
+    """
+    """
+    Now here what we are trying to understand is that when we create class attributes, we can access them from 
+    the instance and class level so when we actually instantiate an object from a class, that object ofcourse
+    knows that that attribute it is seeking is not assigned to it and so it actually seeks for the attribute
+    from the class where this attribute is said to be a class attribute which can be accessed by both
+    the instances and classes as we tested in the code below 
+    """
     pay_rate = 0.8 #pay rate after 20% discount #this is a class attribute
     def __init__(self,name:str,price:float,quantity = 0):#constructor
 
@@ -10,12 +24,32 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self):
+    def calculate_total_price(self):#action performed on object
         return self.price * self.quantity
 
-item1 = Item("Phone",150,5)
-print(item1.pay_rate)
-print(item1.name)
-print(item1.quantity)
-print(item1.calculate_total_price())
-print(Item.pay_rate)
+    #CREATE METHOD THAT WILL GRANT DISCOUNT ON OUR ITEM PRICES
+    def apply_discount(self):#action performed on object
+        #here we are basically applying a discount on our item prices and this happens only on the prices and when the apply_discount() method is
+        #called
+        self.price = self.price * self.pay_rate   #we accessed the class attributes from the class level(Item.pay_rate) but later changed to object level(self.pay_rate)
+
+        #the above statement is the same as saying self.price = self.price * Item.pay_rate
+
+item1 = Item("Phone",100,1)#Instantiated object from the Item class and therefore composed of the attributes assigned to object after constructor
+item1.apply_discount()
+#print(item1.price)  
+#print(item1.calculate_total_price())
+
+
+#Assign a discount to specific item only such as a laptop in our case
+
+item2 = Item("Laptop",1000,3)
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
+
+""" When dealing with a scenario where we would want to apply a discount at the for a specific product amongst out items
+, it is best practice to actually access the class attribute from an instance level in the case of our 
+apply_discount() method, so that in such a case, if we are to assign a discount for a specific instance,
+we can access the class attribute at the instance level
+"""
